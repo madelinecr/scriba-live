0efe6cc4c1bd95d3d2df174696381dfaf25df81b
dre <dre@dre-X202E.(none)>
2013-10-01 10:36:51 -0700

    cleaning up

--- s1_editor/scriba_live/models/user_model.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define("User", {
-
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING
-  });
-}
