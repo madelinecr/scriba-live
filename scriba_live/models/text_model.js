941c3a93e6f8c3fda53e59f405bdc88c2da3abd1
Michael Pruitt <mpruitt3@mail.csuchico.edu>
2013-11-12 09:58:56 -0800

    Add comments to sequelize models noting fields added automatically

--- scriba_live/models/text_model.js
+++ scriba_live/models/text_model.js
@@ -6,3 +6,9 @@ module.exports = function(sequelize, DataTypes) {
     value: DataTypes.TEXT     // Under mysql, stored off table
   });
 }
+/* Columns added by sequelize:
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT
+  `created_at` DATETIME NOT NULL
+  `updated_at` DATETIME NOT NULL
+  `page_id` INTEGER
+*/
